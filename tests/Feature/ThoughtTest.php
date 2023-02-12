3a4a7ddc84888b9c3feb061160e62d6ab7f02ad3
MAGG <alexandragalarzag@gmail.com>
2023-02-12 16:40:45 +0100

    updated test

--- tests/Feature/ThoughtTest.php
+++ tests/Feature/ThoughtTest.php
@@ -41,4 +41,28 @@ class ThoughtTest extends TestCase
         $this->assertEquals($response->author, 'new author');
         $this->assertEquals($response->image, 'new image');
     }
+
+    public function test_thought_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post(route('store'), [
+            'thought' => 'thought',
+            'author' => 'author',
+            'image' => 'image'
+        ]);
+
+        $thought = Thought::first();
+
+        $this->put(route('update', $thought->id), [
+            'thought' => 'new thought',
+            'author' => 'new author',
+            'image' => 'new image'
+        ]);
+
+        $response = Thought::first();
+        $this->assertEquals($response->thought, 'new thought');
+        $this->assertEquals($response->author, 'new author');
+        $this->assertEquals($response->image, 'new image');
+    }
 }
