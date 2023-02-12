14157418cf8000f2822215a2d0a06edbf25b88d4
MAGG <alexandragalarzag@gmail.com>
2023-02-12 20:08:42 +0100

    forms validations with bootstrap

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <form class="m-5 w-75" action="{{ route('store') }}" method="POST" enctype="multipart/form-data">
+    <form class="m-5 w-75 needs-validation" action="{{ route('store') }}" method="POST" enctype="multipart/form-data"
+        novalidate>
         @csrf
         <div class="mb-3">
             <label for="author" class="form-label">Author</label>
-            <input type="text" class="form-control" name="author" id="author">
+            <input type="text" class="form-control" name="author" id="author" required>
+            <div class="invalid-feedback">Please type an author.</div>
+
         </div>
         <div class="mb-3">
             <label for="thought" class="form-label">Thought</label>
-            <textarea class="form-control" name="thought" id="thought" rows="3"></textarea>
+            <textarea class="form-control" name="thought" id="thought" rows="3" required></textarea>
+            <div class="invalid-feedback">Please type a thought.</div>
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input type="text" class="form-control" name="image" id="image">
+            <input type="text" class="form-control" name="image" id="image" required>
+            <div class="invalid-feedback">Please add an image.</div>
         </div>
         <div class="col-12">
             <button class="btn btn-secondary" type="submit">Create</button>
             <a href="{{ route('home') }}"><button class="btn btn-outline-secondary" type="button">Cancel</button></a>
         </div>
     </form>
+    @include('components.validationjs')
 @endsection
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,24 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <form class="m-5 w-75" action="{{ route('update', ['id' => $thought->id]) }}" method="POST" enctype="multipart/form-data">
+    <form class="m-5 w-75 needs-validation" action="{{ route('update', ['id' => $thought->id]) }}" method="POST"
+        enctype="multipart/form-data" novalidate>
         @method('put')
         @csrf
         <div class="mb-3">
             <label for="author" class="form-label">Author</label>
-            <input type="text" class="form-control" id="author" name="author" value="{{ $thought->author }}">
+            <input type="text" class="form-control" id="author" name="author" value="{{ $thought->author }}" required>
+            <div class="invalid-feedback">Please type an author.</div>
         </div>
         <div class="mb-3">
             <label for="thought" class="form-label">Thought</label>
-            <textarea class="form-control" id="thought" name="thought" rows="3">{{ $thought->thought }}</textarea>
+            <textarea class="form-control" id="thought" name="thought" rows="3" required>{{ $thought->thought }}</textarea>
+            <div class="invalid-feedback">Please type a thought.</div>
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input type="text" class="form-control" name="image" id="image" value="{{ $thought->image }}">
+            <input type="text" class="form-control" name="image" id="image" value="{{ $thought->image }}" required>
+            <div class="invalid-feedback">Please add an image.</div>
         </div>
         <div class="col-12">
             <button class="btn btn-secondary" type="submit">Edit</button>
             <a href="{{ route('home') }}"><button class="btn btn-outline-secondary" type="button">Cancel</button></a>
         </div>
     </form>
+    @include('components.validationjs')
 @endsection
