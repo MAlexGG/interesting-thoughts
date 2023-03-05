c917a1a28817f830aa76484535d26e16feb342b1
MAGG <alexandragalarzag@gmail.com>
2023-03-05 12:52:26 +0100

    home route and view  delete and register and redirection changed in controllers

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,14 @@ use App\Http\Controllers\ThoughtController;
 |
 */
 
+Route::get('/', [ThoughtController::class, 'index'])->name('thoughts');
+Route::get('/search', [ThoughtController::class, 'searchByAuthor'])->name('search');
+
 Auth::routes();
 
-Route::get('/', [ThoughtController::class, 'index'])->name('thoughts');
 Route::get('/thoughts/create', [ThoughtController::class, 'create'])->name('create')->middleware('auth');
 Route::post('/thoughts/store', [ThoughtController::class, 'store'])->name('store')->middleware('auth');
 Route::get('/thoughts/{id}', [ThoughtController::class, 'show'])->name('show');
 Route::get('/thoughts/{id}/edit', [ThoughtController::class, 'edit'])->name('edit')->middleware('auth');
 Route::put('/thoughts/{id}', [ThoughtController::class, 'update'])->name('update')->middleware('auth');
 Route::delete('/thoughts/{id}', [ThoughtController::class, 'destroy'])->name('delete')->middleware('auth');
-Route::get('/search', [ThoughtController::class, 'searchByAuthor'])->name('search');
-
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
