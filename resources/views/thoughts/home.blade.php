c21415ecc97a9156213cdda6d3cce679219329da
MAGG <alexandragalarzag@gmail.com>
2023-03-07 14:51:13 +0100

    favorite styles

--- resources/views/thoughts/home.blade.php
+++ resources/views/thoughts/home.blade.php
@@ -12,28 +12,36 @@
                     <a href="{{ route('show', ['id' => $thought->id]) }}">
                         <p class="txt-author">{{ Str::limit($thought->author, 20, ' (...)') }}</p>
                     </a>
-                    @if (Auth::user() && Auth::user()->id == $thought->user_id)
-                        <div class="ct-tought-bt">
-                            <a href="{{ route('edit', ['id' => $thought->id]) }}"><button
-                                    class="btn btn-outline-secondary">Edit</button></a>
-                            <form action="{{ route('delete', ['id' => $thought->id]) }}" method="POST">
-                                @method('delete')
-                                @csrf
-                                <button class="btn btn-outline-danger">Delete</button>
-                            </form>
-                        </div>
-                    @endif
 
-                    @if (!Auth::user())
-                        <a href="{{ route('login') }}">fav</a>
-                    @elseif (Auth::user() && $thought->favorites->contains('user_id', Auth::user()->id))
-                        <button type="text" disabled>favorite</button>
-                    @else
-                        <form action="/thoughts/{{ $thought->id }}/favorites" method="POST">
-                            @csrf
-                            <button type="submit">favorite</button>
-                        </form>
-                    @endif
+                    <div class="d-flex gap-5">
+                        @if (Auth::user() && Auth::user()->id == $thought->user_id)
+                            <div class="ct-tought-bt">
+                                <a href="{{ route('edit', ['id' => $thought->id]) }}"><button
+                                        class="btn btn-outline-secondary">Edit</button></a>
+                                <form action="{{ route('delete', ['id' => $thought->id]) }}" method="POST">
+                                    @method('delete')
+                                    @csrf
+                                    <button class="btn btn-outline-danger">Delete</button>
+                                </form>
+                            </div>
+                        @endif
+
+                        @if (!Auth::user())
+                            <a href="{{ route('login') }}"><button class="btn btn-outline-danger"><img
+                                        src="{{ asset('img/heart_stroke.svg') }}" alt="not-favorite"
+                                        class='img-favorite'></button></a>
+                        @elseif (Auth::user() && $thought->favorites->contains('user_id', Auth::user()->id))
+                            <button type="text" disabled class="btn btn-outline-danger"><img
+                                    src="{{ asset('img/heart.svg') }}" alt="favorite" class='img-favorite'></button>
+                        @else
+                            <form action="/thoughts/{{ $thought->id }}/favorites" method="POST">
+                                @csrf
+                                <button type="submit" class="btn btn-outline-danger"><img
+                                        src="{{ asset('img/heart_stroke.svg') }}" alt="not-favorite"
+                                        class='img-favorite'></button>
+                            </form>
+                        @endif
+                    </div>
                 </div>
             @endforeach
         @else
