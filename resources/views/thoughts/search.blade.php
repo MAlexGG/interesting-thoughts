351189c5910fdd12697301cc226b35af55db0737
MAGG <alexandragalarzag@gmail.com>
2023-03-07 16:33:41 +0100

    search view updated

--- resources/views/thoughts/search.blade.php
+++ resources/views/thoughts/search.blade.php
@@ -10,19 +10,45 @@
                     </div>
                     <p class="txt-thought">{{ Str::limit($thought->thought, 140, ' (...)') }}</p>
                     <a href="{{ route('show', ['id' => $thought->id]) }}">
-                        <p class="txt-author">{{ $thought->author }}</p>
+                        <p class="txt-author">{{ Str::limit($thought->author, 20, ' (...)') }}</p>
                     </a>
-                    @if (Auth::user())
-                        <div class="ct-tought-bt">
-                            <a href="{{ route('edit', ['id' => $thought->id]) }}"><button
-                                    class="btn btn-outline-secondary">Edit</button></a>
-                            <form action="{{ route('delete', ['id' => $thought->id]) }}" method="POST">
+
+                    <div class="d-flex gap-5">
+                        @if (Auth::user() && Auth::user()->id == $thought->user_id)
+                            <div class="ct-tought-bt">
+                                <a href="{{ route('edit', ['id' => $thought->id]) }}"><button
+                                        class="btn btn-outline-secondary">Edit</button></a>
+                                <form action="{{ route('delete', ['id' => $thought->id]) }}" method="POST">
+                                    @method('delete')
+                                    @csrf
+                                    <button class="btn btn-outline-danger">Delete</button>
+                                </form>
+                            </div>
+                        @endif
+
+                        @if (!Auth::user())
+                            <a href="{{ route('login') }}">
+                                <button class="btn btn-outline-danger">
+                                    <img src="{{ asset('img/heart_stroke.svg') }}" alt="not-favorite" class='img-favorite'>
+                                </button>
+                            </a>
+                        @elseif (Auth::user() && $thought->favorites->contains('user_id', Auth::user()->id))
+                            <form action="/thoughts/{{ $thought->id }}/favorites" method="POST">
                                 @method('delete')
                                 @csrf
-                                <button class="btn btn-outline-danger">Delete</button>
+                                <button type="submit" class="btn btn-outline-danger">
+                                    <img src="{{ asset('img/heart.svg') }}" alt="favorite" class='img-favorite'>
+                                </button>
                             </form>
-                        </div>
-                    @endif
+                        @else
+                            <form action="/thoughts/{{ $thought->id }}/favorites" method="POST">
+                                @csrf
+                                <button type="submit" class="btn btn-outline-danger">
+                                    <img src="{{ asset('img/heart_stroke.svg') }}" alt="not-favorite" class='img-favorite'>
+                                </button>
+                            </form>
+                        @endif
+                    </div>
                 </div>
             @endforeach
         @else
