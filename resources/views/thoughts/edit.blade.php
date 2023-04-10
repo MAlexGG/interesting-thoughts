a0826bc6597b2a095e72c10b9d26d56ce797304d
MAlexGG <alexandragalarzag@gmail.com>
2023-04-10 20:04:21 +0200

    validations in thought controller

--- resources/views/thoughts/edit.blade.php
+++ resources/views/thoughts/edit.blade.php
@@ -25,5 +25,19 @@
             <a href="{{ route('thoughts') }}"><button class="btn btn-outline-secondary" type="button">Cancel</button></a>
         </div>
     </form>
+
+    @if ($errors->any())
+    <div class="ct-message p-3">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <ul class="m-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+    @endif
+    
     @include('components.validationjs')
 @endsection
