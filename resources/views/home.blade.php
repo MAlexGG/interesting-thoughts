0793d129d1272b3260dd4c15634688e290eedac2
MAlexGG <alexandragalarzag@gmail.com>
2023-04-10 11:13:17 +0200

    home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <a href="{{ route('thoughts') }}" class="text-decoration-none">
+                        <div class="card-header text-end text-dark">x</div>
+                    </a>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
